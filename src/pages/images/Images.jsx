ed88c99a5bac06caf1c423919c2f8acb4ab9c7b1
ropaolle <ropaolle@gmail.com>
2018-01-24 15:47:53 +0100

    Upload images working

--- src/pages/images/Images.jsx
+++ src/pages/images/Images.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Progress, Container, Row, Col } from 'reactstrap';
-import { storage } from '../../utils/firebase';
-import { database } from '../../utils/firebase';
+import { Button, Progress, Container, Row, Col, Alert } from 'reactstrap';
+import { database, storage } from '../../utils/firebase';
 
 /* TODO
 - Ladda upp filer och lagra länk i db.
@@ -9,84 +8,86 @@ import { database } from '../../utils/firebase';
 
 */
 
-// storage.constructor.prototype.putFiles = (filesObj) => {
-//   const filesArr = [...filesObj];
-//   return Promise.all(filesArr.map((file) => {
-//     const uploadTask = storage.child(file.name).put(file);
-//     uploadTask.on('state_changed', (snapshot) => {
-//       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//       console.log(`${file.name} ${progress}%`);
-//     });
-//     return uploadTask;
-//   }));
-// };
+function uploadImages(fileArr, updateState) {
+  return Promise.all(fileArr.map((file, i) => {
+    const uploadTask = storage.child(`images/${file.name}`).put(file);
+    uploadTask.on('state_changed', (/* snapshot */) => {
+    }, (error) => {
+      console.log('Image upload error', error);
+    }, () => {
+      // Save downloadURL in database
+      const { downloadURL, metadata } = uploadTask.snapshot;
+      database.collection('images').doc(metadata.name).set({ downloadURL });
+      // Update page state
+      updateState(i, downloadURL);
+    });
+    return uploadTask;
+  }));
+}
 
 class Images extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploads: [],
+      status: [],
+      images: [],
+      alert: '',
     };
   }
 
+  updateState = (i, downloadURL) => {
+    const { status } = this.state;
+    status[i].uploaded = true;
+    status[i].url = downloadURL;
+    this.setState({ status });
+  }
+
   handleFileUpload = (e) => {
-    const files = e.target.files;
-    console.log(files);
-
-    const filesArr = [...files];
-    const uploads = filesArr.map((file, i) => ({ name: file.name, progress: 0, key: i }));
-    this.setState({ uploads });
-
-    const upload = Promise.all(filesArr.map((file, i) => {
-      const uploadTask = storage.child(`images/${file.name}`).put(file);
-      uploadTask.on('state_changed', (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        uploads[i].progress = progress;
-        this.setState({ uploads });
-      }, (error) => {
-        console.log('error', error);
-      }, () => {
-        const { downloadURL, metadata } = uploadTask.snapshot;
-        console.log('done, url', downloadURL);
-        // console.log('done', uploadTask.snapshot);
-        database.collection('images').doc(metadata.name).set({ downloadURL });
-      });
-      return uploadTask;
-    }));
-
-    // use it!
-    upload.then((metadatas) => {
-      console.log('metadata', metadatas);
+    const files = e.target.files; // console.log(files);
+    const fileArr = [...files];
+    const status = fileArr.map((file, i) => ({ name: file.name, uploaded: false, key: i }));
+    this.setState({ status });
+    uploadImages(fileArr, this.updateState).then((/* metadatas */) => {
+      // All files uploaded
     }).catch((error) => {
-      console.log('erroe', error);
+      console.log('error', error);
     });
   }
 
   render() {
-    const { uploads } = this.state;
+    const { status, alert /* , images */ } = this.state;
 
-    const progress = uploads.map(file => (
-      <Progress striped color="success" value={file.progress} key={file.key}>
-        {file.name} {file.progress}%
-      </Progress>));
+    const progress = () => {
+      const currProg = (status.filter(val => val.uploaded === true).length / status.length) * 100;
+      return (!!status.length && <Progress striped color="success" value={currProg}>
+        {currProg}%</Progress>);
+    };
+
+    const imageList = status.filter(file => file.url !== undefined).map(file => (
+      <Col className="col-4 col-md-3" key={file.key}>
+        <div><img className="thumbnail" src={file.url} alt={file.name} /></div>
+        <div>{file.name}</div>
+      </Col>));
+    // console.log('status', status);
 
     return (
       <Container fluid className="page-content">
+        <div className="progress-wrapper">{progress()}</div>
+
+        {alert && <Alert color="info">This is a info alert — check it out!</Alert>}
+
         <Row>
-          <Col md="6"><h1>Bilder</h1></Col>
-          <Col md="6">
-            <Form>
-              <FormGroup className="file-upload">
-                <label className="btn btn-success btn-file" htmlFor="fileUpload">
+          <Col className="col-6"><h1>Bilder</h1></Col>
+          <Col className="col-6">
+            <label className="btn btn-success btn-file-upload" htmlFor="fileUpload">
                 Ladda upp bilder
-                  <input type="file" onChange={this.handleFileUpload} id="fileUpload" multiple />
-                </label>
-              </FormGroup>
-            </Form>
+              <input type="file" onChange={this.handleFileUpload} id="fileUpload" multiple />
+            </label>
+            <Button className="show-images" color="primary">Visa alla bilder</Button>{' '}
           </Col>
         </Row>
 
-        {progress}
+        <Row>{imageList}</Row>
       </Container>
     );
   }
